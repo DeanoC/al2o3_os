0d0e500210053ff47bf5a2dbf76ac2edcc488084
Deano <me@deanoc.com>
2019-10-04 17:24:00 +0300

    Major refactor of Mac gui app. No xib/nib used now Now always uses actual display resolution (no backing scale now) Fixes display on MacOs 10.15 (catalana) Start on metal backend only for programatical gpu captures Hermit supports gpu capture on Tab press Fixes catalina macros Removed al2o3_os from al2o3_mutex it now has its own set of mutex functions

--- include/al2o3_os/thread.hpp
+++ include/al2o3_os/thread.hpp
@@ -1,12 +1,9 @@
 // Full license at end of file
-// Summary: Apache - original from The-Forge modified by DeanoC
+// Summary: MIT
 
 #pragma once
-#ifndef WYRD_OS_THREAD_HPP
-#define WYRD_OS_THREAD_HPP
-
-#include "core/core.h"
-#include "os/thread.h"
+#include "al2o3_platform/platform.h"
+#include "al2o3_os/thread.h"
 
 namespace Os {
 
@@ -64,27 +61,3 @@ struct Thread {
 };
 
 }; // end Os namesapce
-#endif
-/*
- * Copyright (c) 2018-2019 Confetti Interactive Inc.
- *
- * This file is part of The-Forge
- * (see https://github.com/ConfettiFX/The-Forge).
- *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
-*/
