b3965acc7857509762f7e6bff62d232cf3437e5e
Dean Calver <me@deanoc.com>
2020-01-23 11:18:01 +0200

    Update al2o3_file added accel_sycl.* to alifetui

--- include/al2o3_os/file.h
+++ include/al2o3_os/file.h
@@ -1,9 +1,6 @@
-// Full license at end of file
-// Summary: Apache - original from The-Forge modified by DeanoC
+// License Summary: MIT see LICENSE file
 
 #pragma once
-#ifndef AL2O3_OS_FILE_H
-#define AL2O3_OS_FILE_H
 
 #include "al2o3_platform/platform.h"
 
@@ -40,29 +37,3 @@ AL2O3_EXTERN_C bool Os_FileSeek(Os_FileHandle handle, int64_t offset, enum Os_Fi
 AL2O3_EXTERN_C int64_t Os_FileTell(Os_FileHandle handle);
 AL2O3_EXTERN_C size_t Os_FileSize(Os_FileHandle handle);
 AL2O3_EXTERN_C bool Os_FileIsEOF(Os_FileHandle handle);
-
-#endif //AL2O3_OS_FILE_H
-
-/*
- * Copyright (c) 2018-2019 Confetti Interactive Inc.
- *
- * This file is part of The-Forge
- * (see https://github.com/ConfettiFX/The-Forge).
- *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
-*/
--- include/al2o3_os/file.hpp
+++ include/al2o3_os/file.hpp
@@ -2,12 +2,10 @@
 // Summary: Apache - original from The-Forge modified by DeanoC
 
 #pragma once
-#ifndef WYRD_OS_FILE_HPP
-#define WYRD_OS_FILE_HPP
 
-#include "core/core.h"
-#include "os/file.h"
-#include "tinystl/string.h"
+#include "al2o3_platform/platform.h"
+#include "al2o3_os/file.h"
+#include "al2o3_tinystl/string.hpp"
 
 namespace Os {
 
@@ -34,28 +32,3 @@ struct File {
 };
 
 } // end Os namespace
-
-#endif //WYRD_OS_FILE_HPP
-/*
- * Copyright (c) 2018-2019 Confetti Interactive Inc.
- *
- * This file is part of The-Forge
- * (see https://github.com/ConfettiFX/The-Forge).
- *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
-*/
